c04bca342bca3eb93e1671c4860556dd61a6e60a
snrredoy <snrredoy3@gmail.com>
2025-06-21 11:31:59 +0600

    use path and query parameter

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class Role(str, Enum):
+    admin = 'admin'
+    manager = 'manager'
+    hr = 'hr'
+    staff = 'staff'
 
 app= FastAPI()
 
+# Simple hello
 @app.get('/')
 async def root():
     return{
         'message': 'Hello World'
     }
 
+
+# User info with path
 @app.get('/info/')
 async def info():
     return{
@@ -17,8 +27,46 @@ async def info():
         'Intake': 36
     }
 
+
+# using path parameter
 @app.get('/rest/{rest_id}')
-async def rest(rest_id):
+async def rest(rest_id: int):
     return {
         'id': rest_id
     }
+
+
+# using path parameter and enum
+@app.get('/role/{role}')
+async def role(role: Role):
+    if role is Role.admin:
+        return {'role': role, 'message': 'Role is admin.'}
+    if role.value == 'manager':
+        return {'role': role, 'message': 'Role is manager.'}
+    if role is Role.hr:
+        return {'role': role, 'message': 'Role is hr.'}
+    if role is Role.staff:
+        return {'role': role, 'message': 'Role is staff.'}
+
+
+# use required query parameter with slice 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Foot"}, {"item_name": "Bart"}, {"item_name": "Bazt"}]
+@app.get('/item/')
+def items(skip: int, limit: int):
+    return fake_items_db[skip : skip + limit]
+
+
+# use optional and default query parameter with slice
+@app.get('/items/')
+def items(skip: int=0, limit: int=10):
+    return fake_items_db[skip : skip + limit]
+
+# use optional query parameter
+@app.get("/optionalQuery/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+# use query parameter type conversion
